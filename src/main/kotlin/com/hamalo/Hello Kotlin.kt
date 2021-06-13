4f69c04b43ef9ee6d58a5279499fabd5c8ea92f7
hamalo-hub <aaron.karmatw@gmail.comm>
2021-06-13 23:54:28 +0800

    Initial commit

---
+++ src/main/kotlin/com/hamalo/Hello Kotlin.kt
@@ -0,0 +1,14 @@
+package com.hamalo
+
+fun main() {
+    //println("hello kotlin")
+    //Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello world")
+    }
+}
